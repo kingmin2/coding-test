1b46d0af1828d1efe99a96869982013537bfa0f4
kingmin2 <meanjeong92@gmail.com>
2022-03-21 23:08:56 +0900

    코딩테스트

---
+++ src/twoPoint/Test01.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Main {	
+	public ArrayList<Integer> solution (int n, int m, int[] arr1, int[]arr2){
+		ArrayList<Integer> answer = new ArrayList<Integer>();
+		
+		if( )
+		 
+		return answer;
+	}
+
+	public static void main(String[] args){
+		Main T = new Main();
+		Scanner sc = new Scanner(System.in);
+		int n=sc.nextInt();
+		int[] arr1 = new int[n];
+		for(int i =0; i<n;i++) {
+			arr1[i] = sc.nextInt();
+		}
+		int m=sc.nextInt();
+		int[] arr2 = new int[m];
+		for(int i =0; i<m;i++) {
+			arr2[i] = sc.nextInt();
+		}
+		 
+		for(int ob : T.solution(n, m, arr1, arr2){
+			System.out.print(ob + " ");
+		}
+	}
+}
